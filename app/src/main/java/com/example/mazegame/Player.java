f707efbe130f0c9f231133b891d3d81da6f2a685
Bebik228 <rakalisa09@gmail.com>
2025-05-24 01:06:43 +0300

    Player.java

--- app/src/main/java/com/example/mazegame/Player.java
+++ app/src/main/java/com/example/mazegame/Player.java
@@ -26,38 +26,3 @@ public class Player {
         this.angle += deltaAngle;
     }
 }
-
-
-
-
-//package com.example.mazegame;
-//
-//public class Player {
-//    public double x;
-//    public double y;
-//    public double angle;
-//
-//    public Player(double x, double y, double angle) {
-//        this.x = x;
-//        this.y = y;
-//        this.angle = angle;
-//    }
-//
-//    public void move(double distance, Maze maze) {
-//        double newX = x + Math.cos(angle) * distance;
-//        double newY = y + Math.sin(angle) * distance;
-//        int gridX = (int) newX;
-//        int gridY = (int) newY;
-//
-//        // Add a check for maze boundaries
-//        if (maze.getCell(gridX, gridY) == 0 && gridX >= 0 && gridX < maze.getWidth() && gridY >= 0 && gridY < maze.getHeight()) {
-//            x = newX;
-//            y = newY;
-//        }
-//    }
-//
-//    public void rotate(double deltaAngle) {
-//        this.angle += deltaAngle;
-//    }
-//}
-
